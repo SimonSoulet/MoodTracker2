0c353e960ee4a1a02b89c3d8a34a36385a7bc85c
Simon <simon.soulet@gmail.com>
2018-04-06 14:20:13 +0200

    Creation of HistoryActivity and Architecture MVC

--- app/src/main/java/com/soulet/simon/moodtracker2/controller/HistoryActivity.java
+++ app/src/main/java/com/soulet/simon/moodtracker2/controller/HistoryActivity.java
@@ -1,13 +1,15 @@
-package com.soulet.simon.moodtracker2;
+package com.soulet.simon.moodtracker2.controller;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.soulet.simon.moodtracker2.R;
+
+public class HistoryActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_history);
     }
 }
