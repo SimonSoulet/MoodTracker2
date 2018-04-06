6ebdb397e4a35b5536a8525cc699784249082c96
Simon <simon.soulet@gmail.com>
2018-04-06 15:05:18 +0200

    Creation of the class SaveMoodReceiver which extends of BroadcastReceiver (Package utils)

---
+++ app/src/main/java/com/soulet/simon/moodtracker2/utils/SaveMoodReceiver.java
@@ -0,0 +1,16 @@
+package com.soulet.simon.moodtracker2.utils;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+/**
+ * Created by Simon on 06/04/2018.
+ */
+
+public class SaveMoodReceiver extends BroadcastReceiver{
+    @Override
+    public void onReceive(Context context, Intent intent) {
+
+    }
+}
